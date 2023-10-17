b4450f5a0d6c32e20d44782479377fb96940fd7b
川音리오 <61252570+KawaneRio@users.noreply.github.com>
2023-10-17 15:36:21 +0900

    Added WanaKana.js

--- script.js
+++ script.js
@@ -1,13 +1,8 @@
-document.getElementById('textForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevent the form from submitting and refreshing the page
+const japaneseInput = document.getElementById('japaneseInput');
+const result = document.getElementById('result');
 
-    // Get the input text value
-    const inputText = document.getElementById('inputText').value;
-
-    // Convert 'a' to 'あ'
-    const convertedText = inputText.replace(/a/g, 'あ');
-
-    // Display the converted text
-    const outputDiv = document.getElementById('output');
-    outputDiv.textContent = `Converted Text: ${convertedText}`;
+japaneseInput.addEventListener('input', function() {
+    const japaneseText = japaneseInput.value;
+    const latinText = wanakana.toRomaji(japaneseText);
+    result.textContent = latinText;
 });
